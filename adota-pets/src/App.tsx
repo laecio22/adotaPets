e0636e42c030fd9fcfc5f69673a51520689d5668
laecio22 <laecon22@hotmail.com>
2025-04-27 13:50:29 -0300

    feat:inicio implementação das  rotas  e  navbar  da  aplicação

--- adota-pets/src/App.tsx
+++ adota-pets/src/App.tsx
@@ -1,12 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Navbar from "./components/Navbar/Navbar";
+import Loading from "./helpers/Loading";
+import { lazy, Suspense } from "react";
+import NotFound from "./components/NotFound/NotFound";
+
+const Dashboard = lazy(()=>import('./components/Dashboard/Dashboard') )
 
 function App() {
-
   return (
-    <div> 
-      <h1 className="font-bold text-4xl">Página  inicial</h1>
-     
-    </div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+         <Route path="/" element={<Suspense fallback={<Loading/>}><Dashboard/></Suspense>}></Route>
+         <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
