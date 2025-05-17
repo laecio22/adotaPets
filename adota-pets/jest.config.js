cad701438bf3d7f4f7f3427c144f2426f3a79714
laecio22 <laecon22@hotmail.com>
2025-05-17 13:30:00 -0300

    fix:Ajuste  no  arquivo de  configuração  do Jest,  porque  estava  ocorrendo  na execução  dos  testes unitários  e  implementação  restante  de  validação  no  teste  do  Navbar

---
+++ adota-pets/jest.config.js
@@ -0,0 +1,15 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.jest.json'
+    }]
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}; 
--- adota-pets/src/tests/Navbar.spec.tsx
+++ adota-pets/src/tests/Navbar.spec.tsx
@@ -1,10 +1,14 @@
-import {render, screen} from "@testing-library/react"
-import Navbar from "../components/Navbar/Navbar"
+import { render, screen } from "@testing-library/react";
+import Navbar from "../components/Navbar/Navbar";
 
-describe('Navbar', ()=>{
-    it('Renderização  do componente  Navbar', ()=>{
-       render(<Navbar/>)
-       screen.getByText("Dashboard")
-       
-    })
-})
+describe("Navbar", () => {
+  it("Renderização  do componente  Navbar", () => {
+    render(<Navbar />);
+    screen.getByText("Dashboard");
+    screen.getByText("Listagem de Animais");
+    screen.getByText("Adoções");
+    screen.getByText("Voluntários");
+    screen.getByText("Eventos");
+    screen.getByText("Configurações");
+  });
+});
