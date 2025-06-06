1a14bd821a3d1e9ae4c0a872b9c17a4c215aa198
laecio22 <laecon22@hotmail.com>
2025-06-06 14:57:46 -0300

    chore:instalação lib  clsx  para  tailwind

--- adota-pets/src/components/Navbar/Navbar.tsx
+++ adota-pets/src/components/Navbar/Navbar.tsx
@@ -4,10 +4,9 @@ import { TbReplace } from "react-icons/tb";
 import { BsPeople } from "react-icons/bs";
 import { MdOutlineEventAvailable } from "react-icons/md";
 import { GrConfigure } from "react-icons/gr";
-import { NavLink} from "react-router";
+import { NavLink } from "react-router";
 import { FaBars, FaXmark } from "react-icons/fa6";
 import { useState } from "react";
-
 
 const itemsNavbar = [
   {
@@ -45,19 +44,20 @@ const itemsNavbar = [
   },
 ];
 const Navbar = () => {
-  const [openMenu, setOpenMenu] =  useState(false)
-  
+  const [openMenu, setOpenMenu] = useState(false);
 
   return (
-    <nav className=" bg-gray-800 text-white pt-8 flex justify-center  ">
+    <nav className=" bg-gray-800 text-white pt-8 flex   ">
       <ul className="w-full  ">
         {itemsNavbar.map((item) => (
           <li key={item.label}>
             <NavLink
               to={item.url}
-              className = {({isActive}) => {
-              return   isActive ? " text-sky-600  mt-10 flex gap-2 h-20 items-center pl-12 transition duration-300 cursor-pointer" : "mt-10 flex gap-2 h-20 items-center   hover:bg-slate-400  pl-12 transition duration-300 cursor-pointer hover:text-sky-200"
-              }}             
+              className={({ isActive }) => {
+                return isActive
+                  ? " text-sky-600  mt-10 flex gap-2 h-20 items-center pl-12 transition duration-300 cursor-pointer"
+                  : "mt-10 flex gap-2 h-20 items-center   hover:bg-slate-400  pl-12 transition duration-300 cursor-pointer hover:text-sky-200";
+              }}
             >
               <span>{item.icon}</span>
               {item.label}
@@ -65,6 +65,13 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <button onClick={() => setOpenMenu(!openMenu)} className="cursor-pointer">
+        {openMenu ? (
+          <FaXmark  />
+        ) : (
+          <FaBars />
+        )}
+      </button>
     </nav>
   );
 };
