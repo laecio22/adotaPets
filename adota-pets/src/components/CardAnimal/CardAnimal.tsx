5e2631f15bcd9e864619a5037e4ca5eb78a31d0f
laecio22 <laecon22@hotmail.com>
2025-08-11 17:19:30 -0300

    feat:inicio  implementação  conteúdo  dos  cards  das  listagens  de  animais

---
+++ adota-pets/src/components/CardAnimal/CardAnimal.tsx
@@ -0,0 +1,9 @@
+
+
+const CardAnimal = () => {
+  return (
+    <div className="h-[350px] w-[30%] bg-amber-900 ">CardAnimal</div>
+  )
+}
+
+export default CardAnimal
--- adota-pets/src/components/ListAnimals/ListAnimals.tsx
+++ adota-pets/src/components/ListAnimals/ListAnimals.tsx
@@ -1,5 +1,6 @@
 import Button from "../../ui/Button/Button";
 import Input from "../../ui/Input/Input";
+import CardAnimal from "../CardAnimal/CardAnimal";
 import { IoAdd } from "react-icons/io5";
 
 const ListAnimals = () => {
@@ -10,17 +11,25 @@ const ListAnimals = () => {
         <div className="mt-4 flex justify-between">
           <Input type="text" name="search" placeholder="Buscar  animal" />
           <Button width="sm:w-[150px] w-56" height="sm:h-18 h-16">
-            <IoAdd  className="h-16 w-8 sm:w-10 sm:h-16" />
+            <IoAdd className="h-16 w-8 sm:w-10 sm:h-16" />
             Adicionar
           </Button>
         </div>
       </header>
-      <section className="bg-white border border-indigo-200 rounded-xl mt-20 min-h-[400px] mx-5  ">
+      <section className="bg-white border border-indigo-200 rounded-xl min-h-[400px] mt-20  mx-5  ">
         <header className=" px-8 py-12">
-          <h1 className="font-semibold text-4xl">Animais para Adoção</h1>         
+          <h1 className="font-semibold text-4xl">Animais para Adoção</h1>
         </header>
-        <div>
-
+        <div className="flex justify-between flex-wrap gap-10 px-8  ">
+          <CardAnimal></CardAnimal>
+          <CardAnimal></CardAnimal>
+          <CardAnimal></CardAnimal>
+           <CardAnimal></CardAnimal>
+          <CardAnimal></CardAnimal>
+          <CardAnimal></CardAnimal>
+           <CardAnimal></CardAnimal>
+          <CardAnimal></CardAnimal>
+          <CardAnimal></CardAnimal>
         </div>
       </section>
     </section>
